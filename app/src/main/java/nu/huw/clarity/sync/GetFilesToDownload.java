0a037c77d52242b231ba32f37db615f2cdcade4e
huw <huw1999@gmail.com>
2016-01-03 18:50:30 +1100

    Add file list downloader
    # Explanation
    GetFilesToDownload is an AsyncTask which heads to the Omni Sync Server and gets a list of all files under /OmniFocus.ofocus/, which are to be downloaded by our client.
    
    It also sifts through the file list and determines which files it actually needs to download. Given the uncontrollable state of the sync format, this is a little tricky, and this method will need to be extended in future. But for our purposes right now, it's pretty okay.

---
+++ app/src/main/java/nu/huw/clarity/sync/GetFilesToDownload.java
@@ -0,0 +1,121 @@
+package nu.huw.clarity.sync;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.jackrabbit.webdav.DavConstants;
+import org.apache.jackrabbit.webdav.DavException;
+import org.apache.jackrabbit.webdav.MultiStatusResponse;
+import org.apache.jackrabbit.webdav.client.methods.DavMethod;
+import org.apache.jackrabbit.webdav.client.methods.PropFindMethod;
+import org.apache.jackrabbit.webdav.property.DavProperty;
+import org.apache.jackrabbit.webdav.property.DavPropertyName;
+import org.apache.jackrabbit.webdav.property.DavPropertySet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import nu.huw.clarity.account.AccountHandler;
+
+/**
+ * Gets a list of files that should be downloaded in this sync. At the moment,
+ * it lists all of the files because of basic validation. But in the future it
+ * should be able to only download changed files.
+ */
+public class GetFilesToDownload extends AsyncTask<Void, Void, List<String>> {
+
+    private static final String TAG = GetFilesToDownload.class.getSimpleName();
+
+    /**
+     * Java callbacks are called listeners, and they allow you to extend a class
+     * which implements this function, which you can then call for sure by
+     * referencing your listener interface. Pretty simple.
+     */
+    public interface TaskListener {
+        void onFinished(List<String> result);
+    }
+
+    private final TaskListener taskListener;
+
+    public GetFilesToDownload(TaskListener listener) {
+        taskListener = listener;
+    }
+
+    /**
+     * Validation for files. Given a WebDAV response, it can determine whether
+     * the file should be downloaded (and also if it isn't a file).
+     *
+     * TODO: Add more logic
+     */
+    private static boolean shouldDownloadFile(MultiStatusResponse response) {
+
+        DavPropertySet properties = response.getProperties(200);
+        DavProperty contentType = properties.get(DavPropertyName.GETCONTENTTYPE);
+
+        // If the content-type header gives us a directory, we're not downloading it
+        if (contentType != null) {
+            if (contentType.getValue().toString().equals("httpd/unix-directory")) {
+
+                return false;
+
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Using the user's data (from Synchroniser's `getHttpClient()`),
+     * we can log into the server and use a WebDAV command to list all
+     * of the files in /OmniFocus.ofocus/ that need to be downloaded.
+     * We only add a file if the validation function gives us a yes.
+     */
+    @Override
+    protected List<String> doInBackground(Void... params) {
+
+        try {
+            List<String> filesToDownload = new ArrayList<>();
+            HttpClient client = Synchroniser.getHttpClient();
+
+            DavMethod listAllFiles = new PropFindMethod(
+                    AccountHandler.getURI(),
+                    DavConstants.PROPFIND_ALL_PROP,
+                    DavConstants.DEPTH_1
+            );
+            client.executeMethod(listAllFiles);
+
+            MultiStatusResponse[] responses = listAllFiles
+                    .getResponseBodyAsMultiStatus()
+                    .getResponses();
+
+            for (MultiStatusResponse response : responses) {
+                if (shouldDownloadFile(response)) {
+
+                    filesToDownload.add(response.getHref());
+
+                }
+            }
+
+            return filesToDownload;
+
+        } catch (IOException | DavException e) {
+            Log.e(TAG, "Problem creating/sending request");
+        }
+
+        return null;
+    }
+
+    /**
+     * Call the callback function, and pass our result to it to be handled.
+     */
+    @Override
+    protected void onPostExecute(List<String> result) {
+        super.onPostExecute(result);
+
+        if (this.taskListener != null) {
+            this.taskListener.onFinished(result);
+        }
+    }
+}
