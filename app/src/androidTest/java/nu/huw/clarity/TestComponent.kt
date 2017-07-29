bbc01a6c27053b9d62687132d6d064a765a74303
Huw <me@huw.nu>
2017-07-29 15:34:14 +1000

    Add a testing database

--- app/src/androidTest/java/nu/huw/clarity/TestComponent.kt
+++ app/src/androidTest/java/nu/huw/clarity/TestComponent.kt
@@ -2,12 +2,11 @@ package nu.huw.clarity
 
 import dagger.Component
 import nu.huw.clarity.dagger.AppModule
-import nu.huw.clarity.dagger.DatabaseModule
 import nu.huw.clarity.db.*
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = arrayOf(AppModule::class, DatabaseModule::class))
+@Component(modules = arrayOf(AppModule::class, DatabaseModuleTest::class))
 interface TestComponent {
     fun inject(test: AttachmentDaoTest)
     fun inject(test: ContextDaoTest)
